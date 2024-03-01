ed06f9bd2cde084a6351850f9d3ac143d5f82091
Qasim1132 <156238211+Qasim1132@users.noreply.github.com>
2024-03-01 15:11:40 +0500

    complete assignment
    assignment complete

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 // // alert("Hello")
 
 
-// let menu = document.querySelector("#menu-bars");
-// let navbar = document.querySelector(".navbar");
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
 
-// menu.onclick = () => {
-//   menu.classList.toggle("fa-times");
-//   navbar.classList.toggle("active");
-// };
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-// let section = document.querySelectorAll("section");
-// let navLinks = document.querySelectorAll("header .navbar a");
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header .navbar a");
 
-// window.onscroll = () => {
-//   menu.classList.remove("fa-times");
-//   navbar.classList.remove("active");
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
 
-//   section.forEach((sec) => {
-//     let top = window.scrollY;
-//     let height = sec.offsetHeight;
-//     let offset = sec.offsetTop - 150;
-//     let id = sec.getAttribute("id");
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute("id");
 
-//     if (top >= offset && top < offset + height) {
-//       navLinks.forEach((links) => {
-//         links.classList.remove("active");
-//         document
-//           .querySelector("header .navbar a[href*=" + id + "]")
-//           .classList.add("active");
-//       });
-//     }
-//   });
-// };
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header .navbar a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
 
 document.querySelector("#search-icon").onclick = () => {
   document.querySelector("#search-form").classList.toggle("active");
@@ -54,6 +54,7 @@ var swiper = new Swiper(".home-slider", {
   },
   loop: true,
 });
+
 var swiper = new Swiper(".review-slider", {
   spaceBetween: 20,
   centeredSlides: true,
@@ -77,3 +78,16 @@ var swiper = new Swiper(".review-slider", {
     },
   },
 });
+
+
+
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
